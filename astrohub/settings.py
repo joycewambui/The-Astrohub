9b1035f33a835619897a5d54886f578737dfcdb3
joycewambui <jwambui@protonmail.com>
2018-05-07 08:44:35 +0300

    Add changes

--- astrohub/settings.py
+++ astrohub/settings.py
@@ -17,6 +17,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
  
+ 
 
 
 # Quick-start development settings - unsuitable for production
@@ -132,10 +133,11 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
-
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- astrophotos/tests.py
+++ astrophotos/tests.py
@@ -6,15 +6,15 @@ from .models import Location,Image
 
 # Create your tests here.
 
-class GalleryTestClass(TestCase):
+class ImageTestClass(TestCase):
     def setUp(self):
-        self.photos=Image(image='image',title='name',description='describe')
+        self.images=Image(image='image',title='name',description='describe')
 #Testign instance
     def test_instance(self):
         self.assertTrue(isinstance(self.images,Image))
 
 #Testing save Method
 def test_save_method(self):
-    self.photos.save_images()
-    gallery = Image.objects.all()
+    self.images.save_images()
+    image = Image.objects.all()
     self.assertTrue(len( image)> 0)
